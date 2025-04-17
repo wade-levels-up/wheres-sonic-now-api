d6413929ca84e2b8f44a1ad1b54056f8348ea244
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-04-17 19:08:05 +1000

    Add executeWithPrisma util
    This utility function executes a database callback
    using the prisma client, if errors occur they're
    logged and then it disconnects the client after
    use to ensure no clients are left open

---
+++ utils/executeWithPrisma.js
@@ -0,0 +1,13 @@
+const prisma = require("../utils/prismaClient");
+
+async function executeWithPrisma(callback) {
+  try {
+    return await callback(prisma);
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await prisma.$disconnect();
+  }
+}
+
+module.exports = { executeWithPrisma };
