102179f5373b09b87ed371104e16620ef4c6a5f1
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-04-22 21:56:44 +1000

    Regenerate session when starting game

--- middlewares/expressSession.js
+++ middlewares/expressSession.js
@@ -2,19 +2,34 @@ require("dotenv").config();
 const session = require("express-session");
 const pg = require("pg");
 const pgSession = require("connect-pg-simple")(session);
+const asyncHandler = require("express-async-handler");
 
 function expressSession() {
   const pgPool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
   });
 
-  return session({
+  const sessionMiddleware = session({
     secret: "key",
     resave: false,
     saveUninitialized: true,
     store: new pgSession({ pool: pgPool, tableName: "Session" }),
     cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 1 Day,
   });
+
+  return (req, res, next) => {
+    sessionMiddleware(req, res, (err) => {
+      if (err) return next(err);
+
+      req.session.regenerate((err) => {
+        if (err) {
+          console.log("Error regenerating session", err);
+          return next(err);
+        }
+        next();
+      });
+    });
+  };
 }
 
 module.exports = expressSession;
