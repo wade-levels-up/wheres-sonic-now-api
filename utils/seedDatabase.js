4cf2c012fa07081d157bb805f9da550dc4248940
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-04-29 20:08:54 +1000

    Update seedDatabase to seed all levels and coords

--- utils/seedDatabase.js
+++ utils/seedDatabase.js
@@ -6,32 +6,8 @@ const seedLevelData = async () => {
 
     // Create levels in Level table
     await executeWithPrisma(async (prisma) => {
+      await prisma.score.deleteMany();
       await prisma.level.deleteMany();
-
-      await prisma.level.create({
-        data: {
-          id: "ice-cap-zone",
-          items: {
-            create: [
-              {
-                name: "sonic",
-                locationX: 2484,
-                locationY: 922,
-              },
-              {
-                name: "tails",
-                locationX: 962,
-                locationY: 1406,
-              },
-              {
-                name: "knuckles",
-                locationX: 2882,
-                locationY: 514,
-              },
-            ],
-          },
-        },
-      });
 
       await prisma.level.create({
         data: {
@@ -50,8 +26,133 @@ const seedLevelData = async () => {
               },
               {
                 name: "knuckles",
-                locationX: 2478,
-                locationY: 918,
+                locationX: 2892,
+                locationY: 514,
+              },
+            ],
+          },
+        },
+      });
+
+      await prisma.level.create({
+        data: {
+          id: "hydro-city-zone",
+          items: {
+            create: [
+              {
+                name: "sonic",
+                locationX: 570,
+                locationY: 1326,
+              },
+              {
+                name: "tails",
+                locationX: 1738,
+                locationY: 1579,
+              },
+              {
+                name: "knuckles",
+                locationX: 214,
+                locationY: 164,
+              },
+            ],
+          },
+        },
+      });
+
+      await prisma.level.create({
+        data: {
+          id: "marble-garden-zone",
+          items: {
+            create: [
+              {
+                name: "sonic",
+                locationX: 472,
+                locationY: 1606,
+              },
+              {
+                name: "tails",
+                locationX: 1714,
+                locationY: 1570,
+              },
+              {
+                name: "knuckles",
+                locationX: 1002,
+                locationY: 792,
+              },
+            ],
+          },
+        },
+      });
+
+      await prisma.level.create({
+        data: {
+          id: "carnival-night-zone",
+          items: {
+            create: [
+              {
+                name: "sonic",
+                locationX: 2092,
+                locationY: 376,
+              },
+              {
+                name: "tails",
+                locationX: 558,
+                locationY: 172,
+              },
+              {
+                name: "knuckles",
+                locationX: 1962,
+                locationY: 1288,
+              },
+            ],
+          },
+        },
+      });
+
+      await prisma.level.create({
+        data: {
+          id: "ice-cap-zone",
+          items: {
+            create: [
+              {
+                name: "sonic",
+                locationX: 2484,
+                locationY: 922,
+              },
+              {
+                name: "tails",
+                locationX: 962,
+                locationY: 1406,
+              },
+              {
+                name: "knuckles",
+                locationX: 82,
+                locationY: 556,
+              },
+            ],
+          },
+        },
+      });
+
+      await prisma.level.create({
+        data: {
+          id: "launch-base-zone",
+          items: {
+            create: [
+              {
+                name: "sonic",
+                locationX: 1082,
+                locationY: 404,
+              },
+              {
+                name: "tails",
+                locationX: 2542,
+                locationY: 1674,
+              },
+              {
+                name: "knuckles",
+                locationX: 964,
+                locationY: 1832,
               },
             ],
           },
