19ebed33dcf98bc3c927c2fe1fdf0086db1ab4c5
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-04-29 17:49:12 +1000

    Destroy session after user submits score

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -50,6 +50,8 @@ const saveUserScore = asyncHandler(async (req, res) => {
       });
     });
 
+    req.session.destroy();
+
     res.status(200).json({ message: "Saved score" });
   } catch (error) {
     throw new Error(error);
