a3f260624fe6c892d5b669dd99d5761cdf8ac2d6
Wade Foster <172018441+wade-levels-up@users.noreply.github.com>
2025-05-05 17:26:13 +1000

    Validate and sanitize name input on score submit

--- controllers/scoresController.js
+++ controllers/scoresController.js
@@ -1,5 +1,15 @@
 const asyncHandler = require("express-async-handler");
 const { executeWithPrisma } = require("../utils/executeWithPrisma");
+const { body, validationResult } = require("express-validator");
+
+const validateUsername = [
+  body("name")
+    .trim()
+    .isLength({ min: 2, max: 12 })
+    .withMessage("Name must be between 2 and 16 characters")
+    .matches(/^\S+$/)
+    .withMessage("Name cannot contain spaces"),
+];
 
 const getUserScore = asyncHandler(async (req, res) => {
   try {
@@ -37,25 +47,39 @@ const getAllLevelScores = asyncHandler(async (req, res) => {
   }
 });
 
-const saveUserScore = asyncHandler(async (req, res) => {
-  try {
-    await executeWithPrisma(async (prisma) => {
-      await prisma.score.update({
-        data: {
-          name: req.body.name,
-        },
-        where: {
-          sid: req.sessionID,
-        },
+const saveUserScore = [
+  validateUsername,
+  asyncHandler(async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        let errorMsgArr = [];
+        let errorMsgString;
+        errors.array().forEach((error) => {
+          errorMsgArr.push(error.msg);
+        });
+        errorMsgString = errorMsgArr.join(", ");
+        throw new Error(errorMsgString);
+      }
+
+      await executeWithPrisma(async (prisma) => {
+        await prisma.score.update({
+          data: {
+            name: req.body.name,
+          },
+          where: {
+            sid: req.sessionID,
+          },
+        });
       });
-    });
 
-    req.session.destroy();
+      req.session.destroy();
 
-    res.status(200).json({ message: "Saved score" });
-  } catch (error) {
-    throw new Error(error);
-  }
-});
+      res.status(200).json({ message: "Saved score" });
+    } catch (error) {
+      throw new Error(error);
+    }
+  }),
+];
 
 module.exports = { getUserScore, getAllLevelScores, saveUserScore };
